9f282d72eefceb79241564f932cf2cc8d110b5a5
OUMAIMA EL GHALBOUNI <elghalbouniomaima@gmail.com>
2026-03-24 12:31:15 +0100

    Refactor navigation and footer data structure

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,180 +3,174 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'About',
+          href: getPermalink('#about'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Skills',
+          href: getPermalink('#skills'),
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Projects',
+          href: getPermalink('#projects'),
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Blog',
+          href: getPermalink(),
         },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  //   {
+  //     text: 'Pages',
+  //     links: [
+  //       {
+  //         text: 'Features (Anchor Link)',
+  //         href: getPermalink('/#features'),
+  //       },
+  //       {
+  //         text: 'Services',
+  //         href: getPermalink('/services'),
+  //       },
+  //       {
+  //         text: 'Pricing',
+  //         href: getPermalink('/pricing'),
+  //       },
+  //       {
+  //         text: 'About us',
+  //         href: getPermalink('/about'),
+  //       },
+  //       {
+  //         text: 'Contact',
+  //         href: getPermalink('/contact'),
+  //       },
+  //       {
+  //         text: 'Terms',
+  //         href: getPermalink('/terms'),
+  //       },
+  //       {
+  //         text: 'Privacy policy',
+  //         href: getPermalink('/privacy'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Landing',
+  //     links: [
+  //       {
+  //         text: 'Lead Generation',
+  //         href: getPermalink('/landing/lead-generation'),
+  //       },
+  //       {
+  //         text: 'Long-form Sales',
+  //         href: getPermalink('/landing/sales'),
+  //       },
+  //       {
+  //         text: 'Click-Through',
+  //         href: getPermalink('/landing/click-through'),
+  //       },
+  //       {
+  //         text: 'Product Details (or Services)',
+  //         href: getPermalink('/landing/product'),
+  //       },
+  //       {
+  //         text: 'Coming Soon or Pre-Launch',
+  //         href: getPermalink('/landing/pre-launch'),
+  //       },
+  //       {
+  //         text: 'Subscription',
+  //         href: getPermalink('/landing/subscription'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Blog',
+  //     links: [
+  //       {
+  //         text: 'Blog List',
+  //         href: getBlogPermalink(),
+  //       },
+  //       {
+  //         text: 'Article',
+  //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+  //       },
+  //       {
+  //         text: 'Article (with MDX)',
+  //         href: getPermalink('markdown-elements-demo-post', 'post'),
+  //       },
+  //       {
+  //         text: 'Category Page',
+  //         href: getPermalink('tutorials', 'category'),
+  //       },
+  //       {
+  //         text: 'Tag Page',
+  //         href: getPermalink('astro', 'tag'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Widgets',
+  //     href: '#',
+  //   },
+  // ],
+  actions: [{ text: 'Download CV', href: getAsset('/resume.pdf'), target: '_blank' }],
 };
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    // {
+    //   title: 'Product',
+    //   links: [
+    //     { text: 'Features', href: '#' },
+    //     { text: 'Security', href: '#' },
+    //     { text: 'Team', href: '#' },
+    //     { text: 'Enterprise', href: '#' },
+    //     { text: 'Customer stories', href: '#' },
+    //     { text: 'Pricing', href: '#' },
+    //     { text: 'Resources', href: '#' },
+    //   ],
+    // },
+    // {
+    //   title: 'Platform',
+    //   links: [
+    //     { text: 'Developer API', href: '#' },
+    //     { text: 'Partners', href: '#' },
+    //     { text: 'Atom', href: '#' },
+    //     { text: 'Electron', href: '#' },
+    //     { text: 'AstroWind Desktop', href: '#' },
+    //   ],
+    // },
+    // {
+    //   title: 'Support',
+    //   links: [
+    //     { text: 'Docs', href: '#' },
+    //     { text: 'Community Forum', href: '#' },
+    //     { text: 'Professional Services', href: '#' },
+    //     { text: 'Skills', href: '#' },
+    //     { text: 'Status', href: '#' },
+    //   ],
+    // },
+    // {
+    //   title: 'Company',
+    //   links: [
+    //     { text: 'About', href: '#' },
+    //     { text: 'Blog', href: '#' },
+    //     { text: 'Careers', href: '#' },
+    //     { text: 'Press', href: '#' },
+    //     { text: 'Inclusion', href: '#' },
+    //     { text: 'Social Impact', href: '#' },
+    //     { text: 'Shop', href: '#' },
+    //   ],
+    // },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Portfolio', href: getPermalink('/') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'www.linkedin.com/in/el-ghalbouni-oumaima-a73a26331' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/elghalbouni-oumaima' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+   Built by Oumaima Elghalbouni · ${new Date().getFullYear()}
   `,
 };
